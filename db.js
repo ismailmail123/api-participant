d796180ea3c0b6c314315be1bebb1adf54c9fbf1
ismail <ismailbary2@gmail.com>
2023-08-07 02:02:02 +0800

    update error

--- db.js
+++ db.js
@@ -6,6 +6,11 @@ const pool = new Pool({
     database: "participant",
     password: "admin",
     port: 5432,
+    errno: -111,
+  code: 'ECONNREFUSED',
+  syscall: 'connect',
+  address: '127.0.0.1',
+  port: 5432
 });
 
 module.exports = pool;
